2449d6314afee2f24c384150fda92c2e2b5d6764
Shaifan2407 <shaifanshaikh13@gmail.com>
2025-02-11 18:31:21 +0530

    uodate validator

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,14 +2,17 @@ const express = require("express");
 const { getUserById, updateUserById } = require("../controller/userController");
 const { authenticate } = require("../middleware/auth");
 const { upload } = require("../middleware/multer");
+const { validateSignUp } = require("../middleware/validator");
 
 const userRouter = express.Router();
 
 userRouter.get("/getUser/:id", authenticate, getUserById);
+
 userRouter.put(
   "/updateUser/:id",
   authenticate,
   upload.single("profilePicture"),
+  validateSignUp,
   updateUserById
 );
 
